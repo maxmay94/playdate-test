b729cc9e704b9cc740c17cc87fd25dba6ddeb162
maxmay94 <maxmay94@gmail.com>
2022-09-13 16:25:57 -0400

    add spikes and movement

--- source/main.lua
+++ source/main.lua
@@ -3,41 +3,22 @@ import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 
 import "circle"
--- import "dvd" -- DEMO
--- local dvd = dvd(1, -1) -- DEMO
+import "spike"
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
-local font = gfx.font.new('font/Mini Sans 2X') -- DEMO
-
-local function loadGame()
-	pd.display.setRefreshRate(50) -- Sets framerate to 50 fps
-	math.randomseed(pd.getSecondsSinceEpoch()) -- seed for math.random
-	gfx.setFont(font) -- DEMO
-end
 
 local function initialize()
-	local circleSprite = Circle(200, 120, 20)
-	circleSprite:add()
-	local circleSprite2 = Circle(100, 220, 10)
-	circleSprite2:add()
-end
-
-local function updateGame()
-	-- dvd:update() -- DEMO
-end
-
-local function drawGame()
-	gfx.clear() -- Clears the screen
-	-- dvd:draw() -- DEMO
-	gfx.sprite.update()
+    local circleSprite = Circle(100, 80, 20)
+    circleSprite:add()
+    local circleSprite2 = Circle(100, 160, 15)
+    circleSprite2:add()
+    local spike = Spike(250, 120)
+    spike:add()
 end
 
 initialize()
-loadGame()
 
-function pd.update()
-	updateGame()
-	drawGame()
-	pd.drawFPS(0,0) -- FPS widget
+function playdate.update()
+    gfx.sprite.update()
 end
